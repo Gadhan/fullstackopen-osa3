06b2dea661a3724825eb6cadfd7bd9e9b730b770
Gadhan <mikaeltikka@hotmail.com>
2024-08-14 14:02:57 +0300

    3.12

--- mongo.js
+++ mongo.js
@@ -4,28 +4,44 @@ if (process.argv.length<3) {
     console.log('give password as argument')
     process.exit(1)
 }
+/*
+if (process.argv.length === 4 || process.argv.length > 5){
+    console.log('wrong number of arguments')
+    process.exit(1)
+}*/
 
 const password = process.argv[2]
 
 const url =
-    `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://gadhan:${password}@cluster0.wbnncub.mongodb.net/noteApp?retryWrites=true&w=majority`
+
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+const phonebookNumberSchema = new mongoose.Schema({
+    name: String,
+    phonenumber: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Phonenumber = mongoose.model('Phonenumber', phonebookNumberSchema)
 
-const note = new Note({
-    content: 'HTML is Easy',
-    important: true,
-})
+if (process.argv.length === 3)
+    Phonenumber.find({}).then(result => {
+        console.log("phonebook:")
+        result.forEach(phonenumber => {
+            console.log(`${phonenumber.name} ${phonenumber.phonenumber}`)
+        })
+        mongoose.connection.close()
+    })
 
-note.save().then(result => {
-    console.log('note saved!')
-    mongoose.connection.close()
-})
+if (process.argv.length === 5){
+    const number = new Phonenumber({
+        name: process.argv[3],
+        phonenumber: process.argv[4]
+    })
+    number.save().then(result => {
+        console.log(`added ${number.name} number ${number.phonenumber} to phonebook`)
+        mongoose.connection.close()
+    })
+}
